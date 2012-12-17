b1fad5c020227ff3dfc2382cf606359deea210a9
kennyvb <kenny@kennyvb.dk>
2012-12-18 00:21:19 +0100

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NiftyFrakklerne::Application.config.secret_token = 'a0f8b220052a13c687d4f23660f201d0ea34b2abecc5b0c4ecf6ca6825a1e64005866d9f09b7a7e406cbc59b8cd4042837d769c546a7d28fb2c18fe6a1e1ca3a'
